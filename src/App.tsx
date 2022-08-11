7f9ca79232c84b2fcebd2eef1e6373b6effb9941
JorikVanRuiswijk <jorik.van.ruiswijk@mediamonks.com>
2022-08-11 15:10:14 +0200

    Add FTC logo svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import FTCLogo from './assets/fancy-type-checker-logo.svg'
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={FTCLogo} alt="Fancy Type Checker logo" />
         <p>
           Fancy Type Checker!
         </p>
