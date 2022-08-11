69f9b7b0f003dd23a4ec34b2fd697eb5f4ecafc1
JorikVanRuiswijk <jorik.van.ruiswijk@mediamonks.com>
2022-08-11 15:28:07 +0200

    Install Sass and add logo and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import FTCLogo from './assets/fancy-type-checker-logo.svg'
-import './App.css';
+import './App.scss';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={FTCLogo} alt="Fancy Type Checker logo" />
-        <p>
-          Fancy Type Checker!
-        </p>
-      </header>
+      <section className="wrapper">
+        <img className='logo' src={FTCLogo} alt="Fancy Type Checker logo" />
+      </section>
     </div>
   );
 }
